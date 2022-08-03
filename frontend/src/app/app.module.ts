e2d87e72325bee0be53fbf751a28add8b32be759
Joseph Chukwuemeke Emefieh <56086064+chukby@users.noreply.github.com>
2022-08-03 23:01:22 +0100

    Update frontend code files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { from } from 'rxjs';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    HttpClientModule,
+    HttpClientModules,
     AppRoutingModule
   ],
   providers: [
