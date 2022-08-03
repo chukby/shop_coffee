17aeafb7b6ffa2964f6b02114cf52ecd94a9cef4
Joseph Chukwuemeke Emefieh <56086064+chukby@users.noreply.github.com>
2022-08-03 23:27:02 +0100

    Update drink-menu.module.ts

--- frontend/src/app/pages/drink-menu/drink-menu.module.ts
+++ frontend/src/app/pages/drink-menu/drink-menu.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angulars';
 
 import { DrinkMenuPage } from './drink-menu.page';
 import { DrinkGraphicComponent } from './drink-graphic/drink-graphic.component';
