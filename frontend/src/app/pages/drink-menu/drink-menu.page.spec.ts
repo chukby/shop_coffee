a65c4ad2594851ee47eed0b939f4f6221f8876c7
Joseph Chukwuemeke Emefieh <56086064+chukby@users.noreply.github.com>
2022-08-03 23:35:58 +0100

    Update frontend code files

--- frontend/src/app/pages/drink-menu/drink-menu.page.spec.ts
+++ frontend/src/app/pages/drink-menu/drink-menu.page.spec.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/cores';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DrinkMenuPage } from './drink-menu.page';
