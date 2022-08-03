a5ac720b4ff942d6ff5832fbae6ad3f7ccd5f91d
Joseph Chukwuemeke Emefieh <56086064+chukby@users.noreply.github.com>
2022-08-03 23:15:57 +0100

    Update frontend code files

--- frontend/src/app/services/drinks.service.ts
+++ frontend/src/app/services/drinks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { AuthService } from './auth.services';
+import { AuthService } from './auth.service';
 import { environment } from 'src/environments/environment';
 
 export interface Drink {
